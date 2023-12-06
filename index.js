e43fd9c16dfa835ae63f3e044db03c0466284a61
pranaygudupally7 <147002789+pranaygudupally7@users.noreply.github.com>
2023-12-06 10:58:01 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,58 +1,107 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const { run, retrieve } = require('./connect');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const {run,retrieve} = require('./connect')
 
-const server = http.createServer(async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  const urlPath = req.url;
-  if (urlPath === '/') {
-    fs.readFile('./index.html', 'UTF-8', function (err, html) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(html);
-    });
-  } else if (urlPath === '/api') {
-    await run();
-    results = await retrieve();
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(results, null, 2))
-  } else if (urlPath.endsWith('.css') || urlPath.endsWith('.js')) {
-    serveStaticFile(res, urlPath, 'text/css');
-  } else if (urlPath.endsWith('.jpg') || urlPath.endsWith('.png')) {
-    serveStaticFile(res, urlPath, `image/${urlPath.endsWith('.jpg') ? 'jpeg' : 'png'}`);
-  } else {
-    res.end('404 Not Found');
-  }
+const server = http.createServer((req, res) => {
+    
+    /*
 
-  console.log(`${req.method} request received for ${req.url}`);
+        
+
+        we can Navigate to different pages via different requests. 
+        if / then goto index.html
+        if /about about then goto about.html
+        if /api then laod the JSON file  /  ;) this might be something you need for your exam. 
+
+
+
+    */
+   
+    
+    
+    if (req.url === '/') {
+        // read public.html file from public folder
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                                    res.end(content);
+                        }
+              );
+     }
+
+    else if (req.url === '/api') {
+        const headers =
+        {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+            "Content-Type": 'application/json'
+        };
+        (async(req,res)=>{
+            try
+            {
+                await run();
+                if (req.method === 'GET') {
+                    var docs_json = await retrieve();
+                    docs_json = JSON.stringify(docs_json, null, 2);
+                    // fs.writeFile('./public/db.json', docs_json, () => {});
+                    res.writeHead(200, headers);
+                    res.end(docs_json);
+                    console.log(docs_json);
+                  }
+                  else {
+                    res.writeHead(405, {'Content-Type': 'text/plain'});
+                    res.end('Method Not supported');
+                  } 
+            }catch (err) {
+                console.error(err);
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end('server failed to handle the request');
+              }
+        })(req,res);
+
+
+        // read the about.html file public folder
+        // fs.readFile(
+        //     path.join(__dirname, 'public', 'about.html'),
+        //             (err, content) => {
+                                    
+        //                             if (err) throw err;
+        //                             res.writeHead(200, { 'Content-Type': 'text/html' });
+        //                             res.end(content);
+        //                 }
+        //       );
+     }
+    else if (req.url==='/api')
+    {
+        fs.readFile(
+            path.join(__dirname, 'public', 'db.json'),'utf-8',
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    // Please note the content-type here is application/json
+                                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                                    res.end(content);
+                        }
+              );
+    }
+    else{
+        res.end("<h1> 404 nothing is here</h1>");
+    }
+
+    /*
+
+        But what if we have  1000 pages/urls ? do we need to write 1000 if-else statements?
+
+    /*/
 });
 
-function serveStaticFile(res, urlPath, contentType) {
-  const filePath = path.join(__dirname, urlPath);
-  const fileStream = fs.createReadStream(filePath);
+// it will first try to look for
+// environment variable, if not found then go for 5959
+const PORT= 5962;
 
-  fileStream.on('error', (error) => {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found');
-  });
-
-  res.on('close', () => {
-    fileStream.destroy();
-  });
-
-  res.writeHead(200, {
-    'Content-Type': contentType,
-    'Cache-Control': 'no-cache',
-  });
-
-  fileStream.pipe(res);
-}
-
-const port = 3009;
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+// port, callback
+server.listen(PORT,()=> console.log(`Great our server is running on port ${PORT} `));
