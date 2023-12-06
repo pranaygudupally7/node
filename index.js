58288c1e77d280be51be2481fb9c3e8315bf30fe
pranaygudupally7 <147002789+pranaygudupally7@users.noreply.github.com>
2023-12-06 11:04:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const {run,retrieve} = require('./connect')
 
 
 const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     
     /*
 
